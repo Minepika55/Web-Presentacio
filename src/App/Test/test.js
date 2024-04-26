28c0737bb6e760712814b547de83eadde05acfc7
Guillem <gmail@gmail.gmail>
2024-04-26 10:32:35 +0200

    NavBar

--- src/App/Test/test.js
+++ src/App/Test/test.js
@@ -2,3 +2,15 @@ function Test(){
     return(<div></div>)
 }
 export default Test
+
+const Navegacio = () => {
+    return (
+      <nav>
+              <ul>
+                  <li>
+                      <a href="/"> Text </a>
+                  </li>
+              </ul>
+          </nav>
+    )
+  }

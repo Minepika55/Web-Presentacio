2acf431ab52ccabe016581e97cb3844fd20b5e00
Guillem <gmail@gmail.gmail>
2024-05-28 23:46:59 +0200

    Pixels

--- src/Extres/index.js
+++ src/Extres/index.js
@@ -5,6 +5,7 @@ import img1 from "./ds1.png";
 import img2 from "./ds2.png";
 import img3 from "./ds3.png";
 import img4 from "./ds4.png";
+import flipnote from "./flipnote.gif";
 
 function Extres() {
   const images = [img1, img2, img3, img4]; //Array amb les imatges
@@ -27,6 +28,12 @@ function Extres() {
     <div className="extres">
       <p>Sóc molt fan de les coses retro. Aqui hi han alguns gifs retro:</p>
       <p>Passa per sobre amb el ratolí per fer zoom.</p>
+      <p>
+        Primer hi ha un partit de futbol que vaig grabar, el gif només utilitza
+        3 colors. Blanc, Negre i Vermell.
+      </p>
+      <p></p>
+      <img src={flipnote} class="pixel" />
       <img src={img} class="zoom" />
       <p></p>
       <img src="img/47.gif" class="zoom" />

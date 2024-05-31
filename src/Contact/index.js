47f7474c84a78bd11d99b5fa1df2157f06a82ef2
Guillem <gmail@gmail.gmail>
2024-05-31 09:15:16 +0200

    Contacte Botons

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -26,7 +26,7 @@ function Contact() {
   }, []);
   return (
     <div className="contact">
-      c
+      <img src={linkedin} id="imatge" />
       <a href="https://www.linkedin.com/in/guillem-ortiz-hern%C3%A0ndez-2061002b7/">
         Linkedin
       </a>
